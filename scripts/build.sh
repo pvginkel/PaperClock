74d9d556c8b661e0c3fffb30c400e3f165aedee9
Pieter van Ginkel <pvginkel@gmail.com>
2024-07-05 07:49:09 +0200

    Fix some paho files keep getting rebuilt.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,10 +10,30 @@ cd build
 
 #if [ -f CMakeCache.txt ]; then rm CMakeCache.txt; fi
 
-if grep -q 'Raspbian' /etc/os-release; then
-  cmake ..
-  make
-else
+if [ "$1" = "debug" ]; then
+  # The version info is rewritten so that it doesn't have a build timestamp
+  # in it. This prevents some files from getting rebuilt on every run.
+
+  CURRENT_VERSIONINFO=$(cat ../lib/paho/src/VersionInfo.h.in)
+  NEW_VERSIONINFO="
+#ifndef VERSIONINFO_H
+#define VERSIONINFO_H
+
+#define BUILD_TIMESTAMP \"develop\"
+#define CLIENT_VERSION  \"@CLIENT_VERSION@\"
+
+#endif /* VERSIONINFO_H */
+"
+
+  if [ "$CURRENT_VERSIONINFO" != "$NEW_VERSIONINFO" ]; then
+    echo "$NEW_VERSIONINFO" > ../lib/paho/src/VersionInfo.h.in
+  fi
+
   cmake -DCMAKE_BUILD_TYPE=Debug ..
   make -j
+else
+  git reset ../lib/paho/src/VersionInfo.h.in >/dev/null 2>&1
+
+  cmake ..
+  make
 fi
