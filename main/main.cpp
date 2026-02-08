7245da1e7411b83b28bc7b948a892326fb46329f
Pieter van Ginkel <pvginkel@gmail.com>
2026-02-08 20:46:30 +0100

    Updated to latest MDM version.

--- main/main.cpp
+++ main/main.cpp
@@ -4,18 +4,13 @@
 #include "esp_task_wdt.h"
 
 extern "C" void app_main(void) {
-    // If we've restarted because of a brownout or watchdog reset,
-    // perform a silent startup.
-    const auto resetReason = esp_reset_reason();
-    const auto silent = resetReason == ESP_RST_BROWNOUT || resetReason == ESP_RST_WDT;
-
     Device device;
 
     device.begin();
 
     Application application(&device);
 
-    application.begin(silent);
+    application.begin();
 
     while (true) {
         vTaskDelay(pdMS_TO_TICKS(1000));
